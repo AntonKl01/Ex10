5bf093b65057ef6e027e1ed801bfab35d0635011
Anton Klykov <antonklykov01@mail.ru>
2021-01-19 20:33:58 +0300

    New5

--- src/postfix.cpp
+++ src/postfix.cpp
@@ -39,37 +39,42 @@ std::string infix2postfix(std::string _str) {
                 is = -1;
                 break;
         }
-        if (is == -1) {
-            _fix.push_back(_str[i]);
-        } else if (is != -2) {
-            if (is == 0) {
-                _fixStack.push(_str[i]);
-                _priorityStack.push(0);
-            } else if (_fixStack.isEmpty()) {
-                if (is == 2) {
+        if (is != -2) {
+            if (is == -1) {
+                _fix.push_back(_str[i]);
+            } else if (is == 0 || _fixStack.isEmpty() ||
+                       is > _priorityStack.get()) {
+                if (is == 0) {
                     _fixStack.push(_str[i]);
-                    _priorityStack.push(2);
-                } else if (is == 3) {
+                    _priorityStack.push(0);
+                } else if (_fixStack.isEmpty()) {
+                    if (is == 2) {
+                        _fixStack.push(_str[i]);
+                        _priorityStack.push(2);
+                    } else if (is == 3) {
+                        _fixStack.push(_str[i]);
+                        _priorityStack.push(3);
+                    }
+                } else if (is > _priorityStack.get()) {
                     _fixStack.push(_str[i]);
-                    _priorityStack.push(3);
+                    _priorityStack.push(is);
                 }
-            } else if (is > _priorityStack.get()) {
-                _fixStack.push(_str[i]);
-                _priorityStack.push(is);
-            } else if (is == 1) {
-                while (_fixStack.get() != '(') {
-                    _fix.push_back(_fixStack.pop());
+            } else {
+                if (is == 1) {
+                    while (_fixStack.get() != '(') {
+                        _fix.push_back(_fixStack.pop());
+                        _priorityStack.pop();
+                    }
                     _priorityStack.pop();
+                    _fixStack.pop();
+                } else if (is <= _priorityStack.get() && is != -2) {
+                    while (is <= _priorityStack.get()) {
+                        _fix.push_back(_fixStack.pop());
+                        _priorityStack.pop();
+                    }
+                    _fixStack.push(_str[i]);
+                    _priorityStack.push(is);
                 }
-                _priorityStack.pop();
-                _fixStack.pop();
-            } else if (is <= _priorityStack.get()) {
-                while (is <= _priorityStack.get()) {
-                    _fix.push_back(_fixStack.pop());
-                    _priorityStack.pop();
-                }
-                _fixStack.push(_str[i]);
-                _priorityStack.push(is);
             }
         }
         i++;
